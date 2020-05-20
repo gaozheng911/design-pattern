533e9b9aaae7b2161733442e2de2c20508983a85
gaoz <zheng.gao@baifendian.com>
2020-05-20 17:00:03 +0800

    策略模式

---
+++ src/com/gz/strategy/OperationSub.java
@@ -0,0 +1,13 @@
+package com.gz.strategy;
+
+/**
+ * @author:gaozheng
+ * @createTime:2020/5/20
+ * @desc:
+ */
+public class OperationSub implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 - num2;
+    }
+}
---
+++ src/com/gz/strategy/Strategy.java
@@ -0,0 +1,10 @@
+package com.gz.strategy;
+
+/**
+ * @author:gaozheng
+ * @createTime:2020/5/20
+ * @desc:
+ */
+public interface Strategy {
+    public int doOperation(int num1,int num2);
+}
